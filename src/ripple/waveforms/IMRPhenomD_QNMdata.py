9db5da3a21375cf3998c45d6c756ad6b81a3f326
tedwards2412 <tedwards2412@gmail.com>
2023-01-30 14:25:53 -0500

    Small updates

--- src/ripple/waveforms/IMRPhenomD_QNMdata.py
+++ src/ripple/waveforms/IMRPhenomD_QNMdata.py
@@ -3030,6 +3030,6 @@ _QNMData_fdamp = jnp.array(
 
 from scipy.interpolate import CubicSpline
 
-QNMData_a = jnp.linspace(-1, 1, 50_000_000)
+QNMData_a = jnp.linspace(-1, 1, 500_000)
 QNMData_fRD = CubicSpline(_QNMData_a, _QNMData_fRD)(QNMData_a)
 QNMData_fdamp = CubicSpline(_QNMData_a, _QNMData_fdamp)(QNMData_a)
